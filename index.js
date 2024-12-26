c5e796602ff02d0bfb092a4ae91acdee1557e645
anandku06 <161126335+anandku06@users.noreply.github.com>
2024-12-26 20:34:51 +0530

    implement initial methods for transaction management and event handling in BudgetTracker class

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ class BudgetTracker{
         this.form = document.getElementById('transactionForm')
         this.transactionList = document.getElementById('transactionList')
         this.balanceElement = document.getElementById('balance')
+
+        this.initEventListeners()
+        this.renderTransactions()
+        this.updateBalance()
     }
+
+    loadTransactions(){
+        return []
+    }
+
+    initEventListeners(){}
+
+    renderTransactions(){}
+
+    deleteTransaction(id){}
+
+    updateBalance(){}
 }
 
 const budgetTracker = new BudgetTracker()
